84bc505d2e6e3fffea5ca1694d440f0629e1dfdf
PipoPcx <juegosonlineps41234@gmail.com>
2023-04-24 21:40:40 -0400

    saltoauto

--- Assets/Scripts/ButtonAnimation.cs
+++ Assets/Scripts/ButtonAnimation.cs
@@ -1,13 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonAnimation : MonoBehaviour
 {
     public Animator animator; // Referencia al componente Animator de tu objeto
+    public Button button; // Referencia al componente Button de tu objeto
 
     void Start()
     {
         // Obtener una referencia al componente Animator de tu objeto
         animator = GetComponent<Animator>();
+
+        // Obtener una referencia al componente Button de tu objeto
+        button = GetComponent<Button>();
+
+        // Agregar un listener al botón para detectar el clic
+        button.onClick.AddListener(OnButtonClick);
     }
 
     public void OnButtonClick()
@@ -16,6 +26,19 @@ public class ButtonAnimation : MonoBehaviour
 
         // Activar la animación
         animator.SetTrigger("SaltoAuto");
+        StartCoroutine(DesactivarAnimacion());
+    }
+
+    IEnumerator DesactivarAnimacion()
+    {
+        // Esperar a que la animación termine
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Activar la animación
+        animator.SetBool("Boton", true);
+
+        // Desactivar la animación
+        animator.SetBool("Boton", false);
     }
 }
 

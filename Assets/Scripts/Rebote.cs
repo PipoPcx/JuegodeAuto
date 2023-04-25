eab86d1f70400ec69e24529c5b2d93bd41189490
PipoPcx <juegosonlineps41234@gmail.com>
2023-04-24 23:11:16 -0400

    actualizacion

--- Assets/Scripts/Rebote.cs
+++ Assets/Scripts/Rebote.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Rebote : MonoBehaviour
 {
     public float fuerza = 10.0f; // La fuerza del rebote
+    public float tiempoImpulso = 1.0f; // Tiempo durante el cual se aplica impulso
+
+    private float tiempoActual = 0.0f; // Tiempo actual transcurrido
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -11,7 +14,21 @@ public class Rebote : MonoBehaviour
             Rigidbody rb = GetComponent<Rigidbody>(); // Obtener el componente Rigidbody del objeto que tiene este script
             Vector3 direccion = collision.contacts[0].normal; // Obtener la dirección de la colisión
             rb.AddForce(direccion * fuerza, ForceMode.Impulse); // Aplicar una fuerza de impulso en la dirección de la colisión
+            tiempoActual = 0.0f; // Reiniciar el tiempo actual
+        }
+    }
+
+    private void Update()
+    {
+        if (tiempoActual < tiempoImpulso) // Si no han pasado suficientes segundos
+        {
+            tiempoActual += Time.deltaTime; // Añadir tiempo transcurrido
+        }
+        else // Si han pasado suficientes segundos
+        {
+            Rigidbody rb = GetComponent<Rigidbody>(); // Obtener el componente Rigidbody del objeto que tiene este script
+            rb.velocity = Vector3.zero; // Detener la velocidad del objeto
+            rb.angularVelocity = Vector3.zero; // Detener la rotación del objeto
         }
     }
 }
-
